103ff10f904c6d7587f427c3285ff4662adc5c8e
Oussama Filani <42185573+filani07@users.noreply.github.com>
2023-03-22 05:47:51 +0000

    add the game logic and style

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NumberGame from "./components/NumberGame";
+
+function App() {
+  return (
+    <div className="App">
+      <NumberGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NumberGame.tsx
@@ -0,0 +1,67 @@
+import React, { useRef, useState } from "react";
+import type { FormEventHandler } from "react";
+import "./style.css";
+
+const randNumber = () => Math.floor(Math.random() * 100) + 1;
+
+export default function NumberGame() {
+  const [userGuess, setUserGuess] = useState(0);
+  const [message, setMessage] = useState("Start guessing...");
+  const [randomNumber, setRandomNumber] = useState<number>(randNumber);
+  const ref = useRef<HTMLDivElement>(null);
+
+  console.log(randomNumber);
+
+  const submitHandler: FormEventHandler = (e) => {
+    e.preventDefault();
+    if (randomNumber === userGuess) {
+      ref.current?.classList.add("winning-bg");
+      setMessage("You win!");
+    } else if (randomNumber < userGuess) {
+      setMessage("Try again! Your guess was too high.");
+    } else {
+      setMessage("Try again! Your guess was too low.");
+    }
+  };
+
+  const restartAgain = () => {
+    setRandomNumber(randNumber);
+    setMessage("Start guessing...");
+    setUserGuess(0);
+    ref.current?.classList.remove("winning-bg");
+  };
+  return (
+    <div className="game" ref={ref}>
+      <header>
+        <h1>Number Game!</h1>
+        <h2>(Between 1 and 100)</h2>
+        <hr />
+        <p>{message}</p>
+      </header>
+
+      <section>
+        <form onSubmit={submitHandler}>
+          <input
+            type="number"
+            name="userGuess"
+            className="text"
+            maxLength={3}
+            min={0}
+            max={100}
+            placeholder="Enter"
+            value={userGuess}
+            onChange={(e) => {
+              setUserGuess(e.target.valueAsNumber);
+            }}
+          />
+          <button type="submit" className="btn">
+            Check
+          </button>
+        </form>
+        <button type="button" className="btn" onClick={restartAgain}>
+          Again
+        </button>
+      </section>
+    </div>
+  );
+}
